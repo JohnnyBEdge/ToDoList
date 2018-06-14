0af642d8f6672a2814a7263a91c9876d258d96a7
John Martin <jrmartin182@gmail.com>
2018-06-14 17:06:53 -0600

    added remove function to button

--- main.js
+++ main.js
@@ -12,21 +12,40 @@ var getFormValue  = document.getElementById('item').value;
 
 //creates new list item
  var newItem = document.createElement('li');
- newItem.className = "list-item";
+ newItem.className = "list-group-item list-group-item-action";
+ newItem.style.display  = 'block';
+//  newItem.id = 'testing';
  newItem.appendChild(document.createTextNode(getFormValue));
+//add li to ul
+getList.appendChild(newItem);
+
 
  //create delete button
  var deleteBtn = document.createElement('button');
  deleteBtn.className = "btn btn-danger btn-sm delete";
  deleteBtn.appendChild(document.createTextNode('X'));
- console.log("hi")
-
  //add delete btn to li
  newItem.appendChild(deleteBtn);
-//add li to ul
-getList.appendChild(newItem);
+
+
+//delete item
+newItem.addEventListener("click", removeItem);
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm("Remove item?")){
+            var li = e.target.parentElement;
+            newItem.remove(li)
+        }
+    }
+};
+
 };
 
 //form submit event
 var form = document.getElementById('addForm');
 form.addEventListener('submit', addItem);
+
+
+
+
