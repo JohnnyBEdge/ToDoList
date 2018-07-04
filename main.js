db54abf02ef0514c5299a4a6fa71611e4fe7be49
John Martin <jrmartin182@gmail.com>
2018-07-04 14:28:00 -0600

    re-did functions, using objects w watchandcode

--- main.js
+++ main.js
@@ -1,72 +1,68 @@
-
-var getList = document.getElementById('items');
-
-
-//adds item to list
-function addItem(e){
-    e.preventDefault();
-
-//gets input value
-var getFormValue  = document.getElementById('item').value;
-
-//creates new list item
- var newItem = document.createElement('li');
- newItem.className = "list-group-item list-group-item";
- newItem.style.display  = 'block';
- 
-//  newItem.id = 'testing';
- newItem.appendChild(document.createTextNode(getFormValue));
-//add li to ul
-getList.appendChild(newItem);
-
-
- //create delete button
- var deleteBtn = document.createElement('button');
- deleteBtn.className = "btn btn-danger btn-sm delete";
- deleteBtn.appendChild(document.createTextNode('X'));
- deleteBtn.style.marginLeft = '10px';
- //add delete btn to li
- newItem.appendChild(deleteBtn);
-
-
-//turns green when clicked
-
-function turnGreen(){
-  if(newItem.classList.contains('checked')){
-    newItem.classList.remove('checked')
-    newItem.style.backgroundColor = 'white'
-    newItem.textContent = getFormValue 
-    
-  } else {
-    newItem.classList.add('checked')
-    newItem.style.backgroundColor = 'green'
-    newItem.textContent = getFormValue +" Completed!";
-  }
-} 
-newItem.addEventListener('click', turnGreen);
-
-
-//delete item
-newItem.addEventListener("click", removeItem);
-
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-        if(confirm("Remove item?")){
-            var li = e.target.parentElement;
-            newItem.remove(li)
+var taskList = {
+    tasks:[],
+    showTasks: function(){
+        if(this.tasks.length === 0){
+            console.log('Your list is empty!')
+        }else{ 
+        console.log('Tasks: ');
+        for(var i = 0; i < this.tasks.length; i++){
+            if (this.tasks[i].completed === true){
+                console.log('( x )', this.tasks[i].taskText);
+            } else {
+                console.log('(  )', this.tasks[i].taskText);
+            }
         }
+        }
+    },
+    addTask: function(taskText){
+        this.tasks.push({
+            taskText: taskText,
+            completed: false
+        });
+        this.showTasks();
+    },
+    changeTask: function(position,taskText){
+        this.tasks[position].taskText = taskText;
+        this.showTasks();
+    },
+    removeTask: function(position){
+        this.tasks.splice(position,1);
+        this.showTasks();
+    },
+    toggleCompleted: function(position){
+        var task = this.tasks[position];
+        task.completed = !task.completed;
+        this.showTasks();
+    },
+
+    toggleAll: function(){
+        var completedTasks = 0;
+        var totalTasks = this.tasks.length;
+
+        //get number completed tasks
+        for( var i = 0; i < totalTasks; i++){
+            if( this.tasks[i].completed === true){
+                completedTasks++;
+            }
+        }
+        //makes true or false
+        if (completedTasks === totalTasks){
+            for(var i = 0; i < totalTasks; i++){
+                this.tasks[i].completed = false;
+            }
+            } else {
+                for(var i = 0; i < totalTasks; i++){
+                    this.tasks[i].completed = true;
+            }
+        }
+            this.showTasks();
+        
     }
-};
+}
+var showTasksBtn = document.getElementById('showTasksBtn');
 
-};
+showTasksBtn.addEventListener('click', function(){
+    taskList.showTasks();
+})
+    
 
-// //form submit event
-var form = document.getElementById('addForm');
-form.addEventListener('submit', addItem);
-
-
-
-
-
-//clicking div highlights div green and adds checkmark
-//click again removes it
