071fa8aa5e977cbabb7453855ffe12cdd81d1e4f
John Martin <jrmartin182@gmail.com>
2018-07-24 20:38:09 -0600

    replaced for loops with foreach

--- main.js
+++ main.js
@@ -1,74 +1,116 @@
 var taskList = {
     tasks:[],
-    showTasks: function(){
-        if(this.tasks.length === 0){
-            console.log('Your list is empty!')
-        }else{ 
-        console.log('Tasks: ');
-        for(var i = 0; i < this.tasks.length; i++){
-            if (this.tasks[i].completed === true){
-                console.log('( x )', this.tasks[i].taskText);
-            } else {
-                console.log('(  )', this.tasks[i].taskText);
-            }
-        }
-        }
-    },
     addTask: function(taskText){
         this.tasks.push({
             taskText: taskText,
             completed: false
         });
-        this.showTasks();
     },
     changeTask: function(position,taskText){
         this.tasks[position].taskText = taskText;
-        this.showTasks();
     },
     removeTask: function(position){
         this.tasks.splice(position,1);
-        this.showTasks();
     },
     toggleCompleted: function(position){
         var task = this.tasks[position];
         task.completed = !task.completed;
-        this.showTasks();
     },
 
     toggleAll: function(){
         var completedTasks = 0;
         var totalTasks = this.tasks.length;
 
-        //get number completed tasks
-        for( var i = 0; i < totalTasks; i++){
-            if( this.tasks[i].completed === true){
+        this.tasks.forEach(function(task){
+            if(task.completed === true){
                 completedTasks++;
             }
-        }
-        //makes true or false
-        if (completedTasks === totalTasks){
-            for(var i = 0; i < totalTasks; i++){
-                this.tasks[i].completed = false;
-            }
-            } else {
-                for(var i = 0; i < totalTasks; i++){
-                    this.tasks[i].completed = true;
-            }
-        }
-            this.showTasks();
+        })
+
         
+        //makes true or false
+        this.tasks.forEach(function(task){ 
+            if(completedTasks === totalTasks){
+                task.completed = false;
+            } else {
+                task.completed = true;
+            }
+            });
+        }
     }
-}
+
 var handlers = {
-    showTasks: function(){
-        taskList.showTasks();
-    },
     toggleAll: function(){
         taskList.toggleAll();
+        view.showTasks();
     },
     addTask: function(){
         var addNewTask = document.getElementById('addNewTask');
         taskList.addTask(addNewTask.value);
         addNewTask.value= "";
+        view.showTasks();
+    }, 
+    changeTask: function(){
+        var changeTaskLocation = document.getElementById('changeTaskLocation');
+        var changeTaskText = document.getElementById('changeTaskText');
+        taskList.changeTask(changeTaskLocation.valueAsNumber,changeTaskText.value)
+        changeTaskLocation.value = "";
+        changeTaskText.value = "";
+        view.showTasks();
+    },
+    removeTask: function(position){
+        taskList.removeTask(position);
+        view.showTasks();
+    }, 
+    toggleCompleted: function(){
+        
+        var toggleCompletedBtn = document.getElementById('toggleCompletedBtn');
+        taskList.toggleCompleted(toggleCompletedBtn.valueAsNumber)
+        view.showTasks();
+    },
+}
+
+var view = {
+    showTasks: function(){
+        var tasksUl = document.querySelector('ul');
+        tasksUl.innerHTML = "";
+        taskList.tasks.forEach(function(task, position){
+            var tasksLi = document.createElement('li');
+
+            var taskCompleted = '';
+
+            if(task.completed === true){
+                taskCompleted = '( X ) '+ task.taskText;
+            } else {
+                taskCompleted = '(  ) '+ task.taskText;
+            }
+            tasksLi.id = position;
+            tasksLi.textContent = taskCompleted;
+            tasksLi.appendChild(this.createDeleteBtn());
+            tasksUl.appendChild(tasksLi);
+        },this)
+
+    },
+    createDeleteBtn: function(){
+        var deleteBtn = document.createElement('button');
+        deleteBtn.textContent = "Delete";
+        deleteBtn.className = "deleteButton";
+        return deleteBtn
+        
+    },
+    setUpEventListeners: function(){
+        var tasksUL = document.querySelector('ul');
+
+        tasksUL.addEventListener('click', function(event){
+            var clickedElement = event.target;
+
+        if(clickedElement.className === 'deleteButton'){
+            handlers.removeTask(parseInt(clickedElement.parentNode.id))
+        }
+        })
+
     }
 }
+    view.setUpEventListeners();
+
+
